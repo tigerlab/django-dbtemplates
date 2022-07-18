07853f2a3672dbe1bb36c84c9217ff60de2d60bc
aimanash <i_man_91@yahoo.com>
2022-07-18 10:29:54 +0800

    [Core][Django] Resolve backward incompatible functions #i2go_django_3

--- dbtemplates/utils/cache.py
+++ dbtemplates/utils/cache.py
@@ -1,3 +1,4 @@
+import django
 from django.core import signals
 from django.contrib.sites.models import Site
 from django.template.defaultfilters import slugify
@@ -11,6 +12,12 @@ def get_cache_backend():
     """
     from django.core.cache import _create_cache
     cache = _create_cache(settings.DBTEMPLATES_CACHE_BACKEND)
+    if django.VERSION[0] >= 3 and django.VERSION[1] >= 2:
+        from django.core.cache import caches
+        cache = caches.create_connection(settings.DBTEMPLATES_CACHE_BACKEND)
+    else:
+        from django.core.cache import _create_cache
+        cache = _create_cache(settings.DBTEMPLATES_CACHE_BACKEND)
     # Some caches -- python-memcached in particular -- need to do a cleanup at
     # the end of a request cycle. If not implemented in a particular backend
     # cache.close is a no-op
